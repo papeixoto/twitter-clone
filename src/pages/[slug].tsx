3bf0b9eadc0219662443028df61e141c6ab88f7c
papeixoto <pazevedopeixoto@gmail.com>
2023-03-25 16:24:09 +0000

    get static props for profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,29 +1,71 @@
-import { type NextPage } from "next";
+import { type GetStaticProps, type NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
 
-const ProfilePage: NextPage = () => {
+type PageProps = {
+  username: string;
+};
+
+const ProfilePage: NextPage<PageProps> = ({ username }) => {
   const { data, isLoading } = api.profile.getUserByUsername.useQuery({
-    username: "papeixoto",
+    username,
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) {
+    console.log("Loading...");
+    return <div>Loading...</div>;
+  }
 
   if (!data) return <div>Unable to load user</div>;
-
-  // log user data
-  console.log(data);
 
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{data.username}</title>
       </Head>
       <main className="flex h-screen justify-center">
         <div>Profile page</div>
+        <div>YO BITCH {username}</div>
       </main>
     </>
   );
+};
+
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
+
+// we're prefetching the data for the page
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("Invalid slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
+
+// passing no paths means paths will be generated at request time
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 };
 
 export default ProfilePage;
