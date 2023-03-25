e232103f354332c7b13a670d18f1510676ff8bac
papeixoto <pazevedopeixoto@gmail.com>
2023-03-25 15:10:35 +0000

    get profile endpoint + some backend refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from "next/app";
+import Head from "next/head";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
@@ -9,6 +10,11 @@ import { Toaster } from "react-hot-toast";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Twitter clone</title>
+        <meta name="description" content="wipwipwip" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
